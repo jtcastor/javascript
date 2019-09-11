b7b8351e933c470da7747d8c7b6b57223b9e3e49
jtcastor <jcastor@entropiinc.com>
2019-09-11 11:32:46 -0600

    initial javascript directory push

---
+++ Data Structures and Algorithms/1 - Big O Notation/runtime-complaxity.js
@@ -0,0 +1,9 @@
+// Big O notation is a mathematical notation that describes the limiting behavior of a function when the argument tends to towards a particular value or Infinity.
+
+// - Use Big O to describe the performance of an algorithm
+
+// O(1) - Constant Time
+// O(n) - Linear Time
+// O(n^2) - Quadratic
+// O(log n) - Logarithmic ***more scalable
+// O(2^n) - Exponential
